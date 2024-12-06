cff6b1e18988b497f4dabae304241c5a66948220
yea <yeweibin@gmail.com>
2024-12-06 09:06:50 +0800

    [update] 调整字段范围绘制方式，完整包含字段的全部比特。

--- modules/assistant-app/src/main/java/m2tk/assistant/app/ui/component/SectionDatagramPanel.java
+++ modules/assistant-app/src/main/java/m2tk/assistant/app/ui/component/SectionDatagramPanel.java
@@ -132,7 +132,6 @@ public class SectionDatagramPanel extends JPanel
                         int end = syntax.getPosition() + (syntax.getBitOffset() + syntax.getBitLength()) / 8;
                         if (end == start || (syntax.getBitOffset() + syntax.getBitLength()) % 8 != 0)
                             end++;
-                        codeArea.setCaretPosition(start, end);
                         codeArea.setSelection(start, end);
                     }
                 }
